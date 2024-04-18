f0601a33e1541ad91c4c509c059435258aec3ee1
Alessandro Corona <alessandro.corona.m@gmail.com>
2024-04-18 10:19:07 +0200

    Initial Commit

---
+++ searchclient/searchclient_python/searchclient/frontier.py
@@ -0,0 +1,147 @@
+from abc import ABCMeta, abstractmethod
+from collections import deque
+from heapq import heappush, heappop
+
+class Frontier(metaclass=ABCMeta):
+    @abstractmethod
+    def add(self, state: 'State'): raise NotImplementedError
+    
+    @abstractmethod
+    def pop(self) -> 'State': raise NotImplementedError
+    
+    @abstractmethod
+    def is_empty(self) -> 'bool': raise NotImplementedError
+    
+    @abstractmethod
+    def size(self) -> 'int': raise NotImplementedError
+    
+    @abstractmethod
+    def contains(self, state: 'State') -> 'bool': raise NotImplementedError
+    
+    @abstractmethod
+    def get_name(self): raise NotImplementedError
+
+class FrontierBFS(Frontier):
+    def __init__(self):
+        super().__init__()
+        self.queue = deque()
+        self.set = set()
+    
+    def add(self, state: 'State'):
+        self.queue.append(state)
+        self.set.add(state)
+    
+    def pop(self) -> 'State':
+        state = self.queue.popleft()
+        self.set.remove(state)
+        return state
+    
+    def is_empty(self) -> 'bool':
+        return len(self.queue) == 0
+    
+    def size(self) -> 'int':
+        return len(self.queue)
+    
+    def contains(self, state: 'State') -> 'bool':
+        return state in self.set
+    
+    def get_name(self):
+        return 'breadth-first search'
+
+class FrontierDFS(Frontier):
+    def __init__(self):
+        super().__init__()
+        self.queue = deque()
+        self.set = set()
+    
+    def add(self, state: 'State'):
+        self.queue.append(state)
+        self.set.add(state)
+
+    def pop(self) -> 'State':
+        state = self.queue.pop()
+        self.set.remove(state)
+        return state
+    
+    def is_empty(self) -> 'bool':
+        return len(self.queue) == 0
+    
+    def size(self) -> 'int':
+        return len(self.queue)
+    
+    def contains(self, state: 'State') -> 'bool':
+        return state in self.set
+    
+    def get_name(self):
+        return 'depth-first search'
+
+class FrontierBestFirst(Frontier):
+    
+    def __init__(self, heuristic: 'Heuristic'):
+        super().__init__()
+        self.heuristic = heuristic
+        self.queue = []
+        self.set = set()
+        self.counter = 0
+    
+    def add(self, state: 'State'):
+        priority = self.heuristic.f(state)
+        heappush(self.queue, (priority, self.counter, state))
+        self.counter += 1
+        self.set.add(state)
+    
+    def pop(self) -> 'State':
+        if not self.queue:
+            return None
+        priority, counter, state = heappop(self.queue)
+        self.set.remove(state)
+        return state    
+    
+    def is_empty(self) -> 'bool':
+        return len(self.queue) == 0
+    
+    def size(self) -> 'int':
+        return len(self.queue)
+    
+    def contains(self, state: 'State') -> 'bool':
+        return state in self.set
+    
+    def get_name(self):
+        return 'best-first search using {}'.format(self.heuristic)
+    
+class FrontierBestFirstWidth(Frontier):
+    
+    def __init__(self, heuristic: 'Heuristic'):
+        super().__init__()
+        self.heuristic = heuristic
+        self.queue = []
+        self.set = set()
+        self.counter = 0
+    
+    def add(self, state: 'State'):
+        # Call w on frontier to update the priority | e.g. if heuristic.w finds out a novel atom w value will drop to 0
+        self.heuristic.get_w(self.queue, state)
+        # Here priority is <w, h>         
+        priority = self.heuristic.f(state)
+        heappush(self.queue, (priority, self.counter, state))
+        self.counter += 1
+        self.set.add(state)
+    
+    def pop(self) -> 'State':
+        if not self.queue:
+            return None
+        priority, counter, state = heappop(self.queue)
+        self.set.remove(state)
+        return state    
+    
+    def is_empty(self) -> 'bool':
+        return len(self.queue) == 0
+    
+    def size(self) -> 'int':
+        return len(self.queue)
+    
+    def contains(self, state: 'State') -> 'bool':
+        return state in self.set
+    
+    def get_name(self):
+        return 'best-first width search <w, h>'.format(self.heuristic)
---
+++ searchclient/searchclient_python/searchclient/heuristic.py
@@ -0,0 +1,232 @@
+from abc import ABCMeta, abstractmethod
+from collections import Counter, deque
+import numpy as np
+import string   
+
+class Heuristic(metaclass=ABCMeta):
+    def __init__(self, initial_state: 'State'):
+        # Here's a chance to pre-process the static parts of the level.
+        self.goal_matrix = np.array(initial_state.goals)
+        self.wall_matrix = np.array(initial_state.walls).astype(int)
+        self.current_agent = None
+        self.current_box = None
+        self.alphabet = string.ascii_uppercase
+        self.latest_state = initial_state
+        self.novelty_sets = set()                             # We assume PDDL tuples
+        self.w = 1
+
+        def atoms(grid, pos):
+            return
+
+        def get_neighbors(grid, pos):
+            
+            neighbors = []
+            rows, cols = len(grid), len(grid[0])
+            x, y = pos
+            
+            # Define offsets for possible movements (up, down, left, right)
+            movements = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+            
+            for dx, dy in movements:
+                nx, ny = x + dx, y + dy
+                # Check if the neighboring position is within the grid boundaries
+                if 0 <= nx < rows and 0 <= ny < cols:
+                    # Check if the neighboring position is not a wall (assuming 0 represents free space)
+                    if grid[nx][ny] == 0:
+                        neighbors.append((nx, ny))
+
+            return neighbors
+
+        def get_path(grid, goal_pos):
+            # Initialize an array to store shortest path distances
+            distances = np.zeros_like(grid)           
+            # Backtrack
+            queue = deque([goal_pos])
+            while queue:
+                current_pos = queue.popleft()
+                for neighbor in get_neighbors(grid, current_pos):
+                    # Populate distances
+                    if distances[neighbor] == 0:
+                        distances[neighbor] = distances[current_pos] + 1
+                        queue.append(neighbor)
+
+            # Fill remaining cells (walls) with 999
+            distances[distances == 0] = 999
+
+            # And return goal position value to 0
+            distances[goal_pos] = 0
+            return distances
+        
+        # Get location of each goal
+
+        goal_positions = []
+
+        # Agents
+        for goal in range(10):
+            goal_pos = np.where(self.goal_matrix == str(goal))
+            if goal_pos[0].size > 0:
+                goal_pos = (int(goal_pos[0][0]), int(goal_pos[1][0]))
+                goal_positions.append((goal, goal_pos))
+        # Boxes
+        for goal in self.alphabet:
+            goal_positions.extend([(goal, (x, y)) for x, y in zip(*np.where(self.goal_matrix == goal))])
+
+        # Get grids on a lookup table
+
+        self.grids = {}
+
+        for goal_name, goal_pos in goal_positions:
+            if type(goal_name) == str:
+                unique_goal_id = f"{goal_name}_({goal_pos[0]}, {goal_pos[1]})"
+                self.grids[unique_goal_id] = get_path(self.wall_matrix, goal_pos)
+            else:
+                self.grids[goal_name] = get_path(self.wall_matrix, goal_pos)
+   
+    # Manhattan
+
+    def manhattan(self, agent, goal):
+        return abs(agent[0] - goal[0]) + abs(agent[1] - goal[1])
+
+    def h(self, state: 'State') -> 'int':
+        
+        count = 0
+        k = 1
+
+        # Goal count 
+
+        '''
+        #counts how many goal cells are not yet covered by an object of the right type
+        count = len(state.agent_rows)
+        for row in range(len(state.goals)):
+            for col in range(len(state.goals[row])):
+                goal = state.goals[row][col]
+                if '0' <= goal <= '9' and (state.agent_rows[ord(goal) - ord('0')] == row and state.agent_cols[ord(goal) - ord('0')] == col):
+                    count -= 1
+        # if(count < 3):
+        #     for row, col in zip(state.agent_rows, state.agent_cols):
+        #         print(row, col)
+        '''  
+
+        ''' SAboxes '''           
+
+        # Default to boxes
+        if any(isinstance(key, str) for key in self.grids.keys()):
+            boxes = []
+            grids = []
+            for x, row in enumerate(state.boxes):
+                for y, box in enumerate(row):
+                    # Filters empty tiles and unassigned boxes
+                    if box != '' and any(key.startswith(box) for key in self.grids.keys()):
+                        box_pos = (x, y)
+                        boxes.append((box, box_pos))
+            boxes = sorted(boxes, key=lambda box: box[0])
+            for key, grid in self.grids.items():
+                grids.append(grid)  
+            # Loop through boxes and grids, alphabetically. Assigns each box to its grid, so it works also when they are not specifically labeled
+            for box, grid in zip(boxes, grids):
+                grid = np.array(grid)
+                ''' If we want to add nearest box heuristic in the mix '''
+                count += self.manhattan((box[1][0], box[1][1]), (state.agent_rows[0], state.agent_cols[0]))
+                count += grid[box[1][0]][box[1][1]] * k
+
+        # Default to pathfinding
+        else:
+            agents = []
+            for agent_num, agent_row in enumerate(state.agent_rows):
+                agent_col = state.agent_cols[agent_num]
+                agent_pos = (agent_row, agent_col)
+                if agent_num in self.grids:
+                    grid = self.grids[agent_num]
+                    # count += grid[agent_pos]
+                    agents.append(grid[agent_pos])
+                    count = max(agents)
+                else: 
+                    count += self.manhattan(agent_pos, (agent_num, agent_num))
+
+        return count
+
+    def get_w(self, frontier, state: 'State') -> 'int':
+        
+        ''' For efficiency,
+        as discussed before, we don’t compute the value
+        w(s) exactly. Instead, except when stated otherwise, we just
+        determine whether w(s) is 1 or greater than 1. As a result, a
+        state s will not be preferred to a state s' when the two states
+        have the same heuristic values and both states have novelty
+        measures greater than 1. 
+        '''
+        w = self.w
+        
+        state_repr = self.atoms(state)
+
+        heuristic_val = self.h(state)
+        filtered_seen_states = [seen_state for seen_state in frontier if self.h(seen_state) == heuristic_val]
+        for seen_state in filtered_seen_states:
+            self.novelty_sets.append(self.atoms(seen_state))      # This should populate the set with a set for each seen state with the same h
+
+        for set in self.novelty_sets:
+            if state_repr - set:        # If one atom is novel
+                w = 0
+                self.w = w
+
+        return w
+
+    @abstractmethod
+    def f(self, state: 'State') -> 'int': pass
+    
+    @abstractmethod
+    def __repr__(self): raise NotImplementedError
+
+class HeuristicAStar(Heuristic):
+    def __init__(self, initial_state: 'State'):
+        super().__init__(initial_state)
+    
+    def f(self, state: 'State') -> 'int':
+        return state.g + self.h(state)
+    
+    def __repr__(self):
+        return 'A* evaluation'
+
+class HeuristicWeightedAStar(Heuristic):
+    def __init__(self, initial_state: 'State', w: 'int'):
+        super().__init__(initial_state)
+        self.w = w
+    
+    def f(self, state: 'State') -> 'int':
+        return state.g + self.w * self.h(state)
+    
+    def __repr__(self):
+        return 'WA*({}) evaluation'.format(self.w)
+
+class HeuristicGreedy(Heuristic):
+    def __init__(self, initial_state: 'State'):
+        super().__init__(initial_state)
+    
+    def f(self, state: 'State') -> 'int':
+        return self.h(state)
+    
+    def __repr__(self):
+        return 'greedy evaluation'
+
+# Greedy BFWS
+
+class HeuristicBFWS(Heuristic):
+    def __init__(self, initial_state: 'State'):
+        super().__init__(initial_state)
+    
+    '''
+    Remarkably, however, BFWS with evaluation function
+    f2 = <w, h>, where the preference order between heuristics
+    and novelties are reversed, performs much better than
+    both BFWS(f1) with f1 = <h,w> and GBFS-W. The preferred
+    states in f2 = <w, h> are not picked among the ones
+    with lowest h but among those with lowest novelty measure
+    w(s) = wh(s), with the heuristic h being used as a
+    tie breaker.
+    '''
+
+    def f(self, state: 'State') -> 'int':
+        return (self.w(state), self.h(state))
+    
+    def __repr__(self):
+        return 'BFWS evaluation'
